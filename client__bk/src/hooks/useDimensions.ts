c22daa66865f284467b870fa74ee314fb7f08727
deepakbhattmits <deepakbhattmits@gmail.com>
2022-06-23 09:35:55 +0530

    Create useDimensions.ts

---
+++ client__bk/src/hooks/useDimensions.ts
@@ -0,0 +1,29 @@
+/** @format */
+
+import { useState, useEffect } from 'react';
+
+const getWindowDimensions=()=> {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+		width,
+		height,
+	};
+}
+
+const useDimensions = () => {
+	const [windowDimensions, setWindowDimensions] = useState(
+		getWindowDimensions()
+	);
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimensions(getWindowDimensions());
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return windowDimensions;
+};
+export default useDimensions;
