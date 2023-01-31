046305fe03219cd7c84129f6add2ad9d78fabb98
deepakbhattmits <deepakbhattmits@gmail.com>
2023-01-31 20:10:22 +0530

    Delete NotFound.tsx

--- client__BK/src/components/NotFound.tsx
+++
@@ -1,4 +0,0 @@
-import {FC} from 'react';
-import {NavLink} from 'react-router-dom'
-const NotFound: FC=():JSX.Element => <h1>Page you are looking is not found, <NavLink to='/'>click here</NavLink> to redirect to Home</h1>;
-export default NotFound;
