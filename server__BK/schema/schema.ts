2e53ca1b6816f8e02fd15ef860c2c7fb8e7da31b
deepakbhattmits <deepakbhattmits@gmail.com>
2023-05-16 23:12:26 +0530

    Delete schema.ts

--- server__BK/schema/schema.ts
+++
@@ -1,7 +0,0 @@
-import {GraphQLSchema} from 'graphql';
-const query = require('./root_query_type')
-const mutation = require('./mutations')
-module.exports=new GraphQLSchema({
-    query,
-    mutation
-})
