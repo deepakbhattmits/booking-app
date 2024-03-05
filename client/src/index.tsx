53266517d203ae8c556eab96f69753fababcb39e
deepakbhattmits <deepakbhattmits@gmail.com>
2024-03-05 15:49:34 +0530

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,15 +9,22 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js'
-import './assets/styles/_index.scss'
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
-const rootElement = document.querySelector('#root')! as HTMLDivElement //  rootElement
-ReactDOM.render(
-  // <React.StrictMode>
-  // <App />
-  // </React.StrictMode>
+} from "chart.js";
+import "./assets/styles/_index.scss";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const rootElement = document.querySelector("#root")! as HTMLDivElement; //  rootElement
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 
-  <App />,
-  rootElement,
-)
+  // <App />
+);
